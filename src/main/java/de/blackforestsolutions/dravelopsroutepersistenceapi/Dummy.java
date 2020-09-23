7e853d385087b8f5269093fd49f6fd1f10fb30c9
hennihaus <jan-hendrik.hausner@outlook.com>
2020-09-23 14:36:48 +0200

    initial commit

---
+++ src/main/java/de/blackforestsolutions/dravelopsroutepersistenceapi/Dummy.java
@@ -0,0 +1,10 @@
+package de.blackforestsolutions.dravelopsroutepersistenceapi;
+
+public class Dummy {
+
+    public static final int TWO = 2;
+
+    public int returnTwo() {
+        return TWO;
+    }
+}
