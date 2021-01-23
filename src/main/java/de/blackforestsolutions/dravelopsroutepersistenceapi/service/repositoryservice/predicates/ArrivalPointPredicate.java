824eed4325046860de629c5e6734a8d3dbf3fa96
hennihaus <jan-hendrik.hausner@outlook.com>
2021-01-23 02:18:47 +0100

    javadocs for MAX_EPSILON

--- src/main/java/de/blackforestsolutions/dravelopsroutepersistenceapi/service/repositoryservice/predicates/ArrivalPointPredicate.java
+++ src/main/java/de/blackforestsolutions/dravelopsroutepersistenceapi/service/repositoryservice/predicates/ArrivalPointPredicate.java
@@ -9,6 +9,9 @@ import java.util.Map;
 public class ArrivalPointPredicate implements Predicate<String, Journey> {
 
     private static final long serialVersionUID = -1434743634655172925L;
+    /**
+     * tolerance value to avoid double overflows
+     */
     private static final double MAX_EPSILON = 0.00001d;
 
     private final Point arrivalPointToCompare;
@@ -22,6 +25,7 @@ public class ArrivalPointPredicate implements Predicate<String, Journey> {
         Point arrivalPoint = entry.getValue().getLegs().getLast().getArrival().getPoint();
 
         // save way for arrivalPoint.getX() == arrivalPointToCompare.getX() && arrivalPoint.getY() == arrivalPointToCompare.getY();
+        // avoids a double overflow
         return Math.abs(arrivalPoint.getX() - arrivalPointToCompare.getX()) < MAX_EPSILON
                 &&
                 Math.abs(arrivalPoint.getY() - arrivalPointToCompare.getY()) < MAX_EPSILON;
--- src/main/java/de/blackforestsolutions/dravelopsroutepersistenceapi/service/repositoryservice/predicates/DeparturePointPredicate.java
+++ src/main/java/de/blackforestsolutions/dravelopsroutepersistenceapi/service/repositoryservice/predicates/DeparturePointPredicate.java
@@ -9,6 +9,9 @@ import java.util.Map;
 public class DeparturePointPredicate implements Predicate<String, Journey> {
 
     private static final long serialVersionUID = -1760994329544417605L;
+    /**
+     * tolerance value to avoid double overflows
+     */
     private static final double MAX_EPSILON = 0.00001d;
 
     private final Point departurePointToCompare;
@@ -21,7 +24,8 @@ public class DeparturePointPredicate implements Predicate<String, Journey> {
     public boolean apply(Map.Entry<String, Journey> entry) {
         Point departurePoint = entry.getValue().getLegs().getFirst().getDeparture().getPoint();
 
-//        save way for departurePoint.getX() == departurePointToCompare.getX() && departurePoint.getY() == departurePointToCompare.getY();
+        // save way for departurePoint.getX() == departurePointToCompare.getX() && departurePoint.getY() == departurePointToCompare.getY();
+        // avoids a double overflow
         return Math.abs(departurePoint.getX() - departurePointToCompare.getX()) < MAX_EPSILON
                 &&
                 Math.abs(departurePoint.getY() - departurePointToCompare.getY()) < MAX_EPSILON;
